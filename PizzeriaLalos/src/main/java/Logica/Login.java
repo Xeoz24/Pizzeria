982d9b762246f0c99b51de195038013a93ecb62d
Eduardo Javier Naal <131737602+EduardoNaal@users.noreply.github.com>
2023-09-25 17:34:34 -0600

    se modifico la clase login del paquete logica

--- PizzeriaLalos/src/main/java/Logica/Login.java
+++ PizzeriaLalos/src/main/java/Logica/Login.java
@@ -9,5 +9,62 @@ package Logica;
  * @author Eduardo
  */
 public class Login {
-    
+
+    private String nombres, apellidos, correo_electronico, num_telofonico, password;
+
+    public Login() {
+    }
+
+    public Login(String nombres, String apellidos, String correo_electronico, String num_telofonico, String password) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo_electronico = correo_electronico;
+        this.num_telofonico = num_telofonico;
+        this.password = password;
+    }
+
+    public void iniciarSesion(String correo_electronico, String num_telofonico, String password) {
+        if(correo_electronico.isEmpty())
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNum_telofonico() {
+        return num_telofonico;
+    }
+
+    public void setNum_telofonico(String num_telofonico) {
+        this.num_telofonico = num_telofonico;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
 }
