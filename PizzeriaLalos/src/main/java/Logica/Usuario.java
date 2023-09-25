64aa9f2198103ae707d50120a0841a4be1ea5332
Xeoz24 <117026367+Xeoz24@users.noreply.github.com>
2023-09-25 18:34:13 -0500

    Agrego la Clase Usuario Le doy los atributos necesarios para la creación de un nuevo usuario

--- PizzeriaLalos/src/main/java/Logica/Usuario.java
+++ PizzeriaLalos/src/main/java/Logica/Usuario.java
@@ -1,9 +1,82 @@
 
 package Logica;
-/**
- *
- * @author fwscg
- */
+ 
 public class Usuario {
+    
+    
+    //Atributos de un usario
+    
+    private String nombres,apellidos,correo_electronico,numero_telefonico, password;
+    private int ID;
+
+    public Usuario(String nombres, String apellidos, String correo_electronico, String numero_telefonico, String password, int ID) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo_electronico = correo_electronico;
+        this.numero_telefonico = numero_telefonico;
+        this.password = password;
+        this.ID = ID;
+    }
+
+    public Usuario() {
+    }
+    
+
+    
+    
+    
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getNumero_telefonico() {
+        return numero_telefonico;
+    }
+
+    public void setNumero_telefonico(String numero_telefonico) {
+        this.numero_telefonico = numero_telefonico;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+    
+    
+    
+    
+    
     
 }
