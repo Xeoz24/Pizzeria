cbfd7e55c05733333ed376cd3d244d3ff02b6ab0
Xeoz24 <117026367+Xeoz24@users.noreply.github.com>
2023-10-01 05:51:00 -0600

    Cambio Grande

---
+++ PizzeriaLalos/target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"GUI"},{"l":"Logica"},{"l":"Logica.Pedidos"},{"l":"Persistencia.ColaCircular"},{"l":"Persistencia.Lista"}];updateSearchResults();
